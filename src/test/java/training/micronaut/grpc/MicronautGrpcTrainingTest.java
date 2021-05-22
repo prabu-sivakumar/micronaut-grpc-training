c27fb799bc21f086a245e4fd4eaf39a810174203
Prabu Varadharajalu Sivakumar <prabu-sivakumar>
2021-05-22 03:05:02 +0000

    Initial commit

---
+++ src/test/java/training/micronaut/grpc/MicronautGrpcTrainingTest.java
@@ -0,0 +1,21 @@
+package training.micronaut.grpc;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+class MicronautGrpcTrainingTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
